ab5c567cc83269053354f113f49336a06a8e761e
Daniel Kim <birmsookim@gmail.com>
2016-05-17 01:52:36 +0000

    import math and create circleTest.py

--- circle.py
+++ circle.py
@@ -1,14 +1,14 @@
-pi = 3.14159
+import math
 
 def area(radius):
-    return pi * (radius**2)
+    return math.pi * (radius**2)
 
 def circumference(radius):
-    return 2*pi*radius
+    return 2*math.pi*radius
 
 def sphereSurface(radius):
     return 4.0*area(radius)
 
 def sphereVolum(radius):
-    return (4.0/3.0)*pi*(radius**3)
+    return (4.0/3.0)*math.pi*(radius**3)
 
